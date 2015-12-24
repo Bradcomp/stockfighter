da10b0d96b45a305507cc6462e6220d478e3c835
Bradcomp <notpmoc84@hotmail.com>
2015-12-23 23:32:39 -0800

    Updated readme, curried all non-curried functions

--- api/sockets.js
+++ api/sockets.js
@@ -1,25 +1,29 @@
 'use strict';
 const WebSocket = require('ws');
 const {URL} = require('../config');
-const {prop, compose} = require('ramda');
+const {prop, compose, curry} = require('ramda');
 
 const connect = (path, onMessage) => {
     let ws = new WebSocket(path);
     ws.on('open', ws =>  console.log(`opened Socket to ${path}`));
     ws.on('message', onMessage);
     return ws;
-}
+};
 
-const ticker = (account, venue, onMessage) => 
+const ticker = curry((account, venue, onMessage) => 
     connect(`${URL}/ws/${account}/venues/${venue}/tickertape`, onMessage)
+);
 
-const stockTicker = (account, venue, stock, onMessage) => 
+const stockTicker = curry((account, venue, stock, onMessage) => 
     connect(`${URL}/ws/${account}/venues/${venue}/tickertape/stocks/${stock}`, onMessage)
+);
 
-const executions = (account, venue, onMessage) => 
+const executions = curry((account, venue, onMessage) => 
     connect(`${URL}/ws/${account}/venues/${venue}/executions`, onMessage)
+);
 
-const stockExecutions = (account, venue, stock, onMessage) =>
+const stockExecutions = curry((account, venue, stock, onMessage) =>
     connect(`${URL}/ws/${account}/venues/${venue}/executions/stocks/${stock}`, onMessage)
+);
 
 module.exports = {ticker, stockTicker, executions, stockExecutions}
--- api/stockinfo.js
+++ api/stockinfo.js
@@ -1,16 +1,17 @@
 'use strict';
-const get = require('./helpers/getrequest')
-const {URL} = require('../config')
+const get = require('./helpers/getrequest');
+const {URL} = require('../config');
+const {curry} = require('ramda');
 
 const getStocks = venue => 
     get(`${URL}/venues/${venue}/stocks`)
       
 
-const getOrderbook = (venue, stock) => 
+const getOrderbook = curry((venue, stock) => 
     get(`${URL}/venues/${venue}/stocks/${stock}`)
-       
+);
 
-const getQuote = (venue, stock) => 
+const getQuote = curry((venue, stock) => 
     get(`${URL}/venues/${venue}/stocks/${stock}/quote`)
-
+);
 
